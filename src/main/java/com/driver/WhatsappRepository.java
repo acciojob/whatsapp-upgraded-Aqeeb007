c0454ccc7fc2ad37d9f87fbef27f94a166455541
Aqeeb <aqebmohammad@gmail.com>
2023-01-29 13:21:48 +0530

    2.0

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -3,12 +3,17 @@ package com.driver;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
+import java.util.List;
 
 @Repository
 public class WhatsappRepository {
 
     HashMap<String,User> userHashMap = new HashMap<>();
-    HashMap<String,Group> groupHashMap = new HashMap<>();
-    HashMap<User,User> userUserHashMap = new HashMap<>();
+    HashMap<Group, List<User>> groupHashMap = new HashMap<>();
+    HashMap<Integer,Message> messageHashMap = new HashMap<>();
+
+    HashMap<Group,Integer> groupIntegerHashMap = new HashMap<>();
+
+
 
 }
--- src/main/java/com/driver/WhatsappService.java
+++ src/main/java/com/driver/WhatsappService.java
@@ -3,6 +3,7 @@ package com.driver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -28,7 +29,7 @@ public class WhatsappService {
 
             User admin = users.get(0);
             group = new Group(admin.getName(), 2);
-            whatsappRepository.groupHashMap.put(admin.getName(),group);
+            whatsappRepository.groupHashMap.put(group,users);
 
         }else{
 
@@ -37,7 +38,7 @@ public class WhatsappService {
 
             String groupName = "Group" + size+1;
             group = new Group(groupName, users.size()+1);
-            whatsappRepository.groupHashMap.put(groupName,group);
+            whatsappRepository.groupHashMap.put(group,users);
             group = new Group(admin.getName(), users.size()+1);
         }
         return group;
@@ -45,11 +46,32 @@ public class WhatsappService {
 
 
     public int createMessage(String content) {
-        return 0;
+        int size = whatsappRepository.messageHashMap.size()+1;
+
+        Message message = new Message();
+
+        message.setId(size);
+        message.setContent(content);
+
+        whatsappRepository.messageHashMap.put(size,message);
+        return size;
     }
 
     public int sendMessage(Message message, User sender, Group group) {
-        return 0;
+        if(!whatsappRepository.messageHashMap.containsKey(message.getId())){
+            throw new RuntimeException("Group does not exist");
+        }
+
+        List<User> member = whatsappRepository.groupHashMap.get(group);
+
+        if(!member.contains(sender)){
+            throw new RuntimeException("You are not allowed to send message");
+        }
+
+        int msg = whatsappRepository.groupIntegerHashMap.get(group);
+        whatsappRepository.groupIntegerHashMap.put(group,msg+1);
+        return msg+1;
+
     }
 
     public String changeAdmin(User approver, User user, Group group) {
@@ -61,6 +83,7 @@ public class WhatsappService {
     }
 
     public String findMessage(Date start, Date end, int k) {
+
         return "empty";
     }
 }
